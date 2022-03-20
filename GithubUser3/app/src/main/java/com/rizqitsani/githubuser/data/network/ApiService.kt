19e06a925721170367c5cbb7b9ff0335210eab8c
rizqitsani <rizqitsani@gmail.com>
2022-03-20 17:01:08 +0700

    chore(s3): move api key to local properties

--- GithubUser3/app/src/main/java/com/rizqitsani/githubuser/data/network/ApiService.kt
+++ GithubUser3/app/src/main/java/com/rizqitsani/githubuser/data/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.rizqitsani.githubuser.data.network
 
+import com.rizqitsani.githubuser.BuildConfig
 import com.rizqitsani.githubuser.data.network.response.FollowerResponse
 import com.rizqitsani.githubuser.data.network.response.FollowingResponse
 import com.rizqitsani.githubuser.data.network.response.SearchUserResponse
@@ -12,25 +13,25 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     fun searchUser(
         @Query("q") q: String
     ): Call<SearchUserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     fun getUserDetail(
         @Path("username") username: String
     ): Call<UserDetailResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     fun getFollowers(
         @Path("username") username: String
     ): Call<List<FollowerResponse>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.API_KEY}")
     fun getFollowing(
         @Path("username") username: String
     ): Call<List<FollowingResponse>>
